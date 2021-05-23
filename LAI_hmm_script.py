779ed4b7d994b8b1411de1b49a52fdf32d056aed
Hannah Mummey <hmmummey@gmail.com>
2021-05-22 17:47:53 -0700

    Added Viterbi algorithm and path reconstruction functionality

--- LAI_hmm_script.py
+++ LAI_hmm_script.py
@@ -1,11 +1,6 @@
 import random as rn
 import numpy as np
 
-### TO DO:
-    # - Refresh on genome representation, change indexing methods as necessary
-    #   (just cut down emission_df and then use the index in SNPseq?)
-    # - Adapt optimal path and reconstruct path for any changes and test!
-
 
 def HammingDist(str1,str2):
     dist = 0
@@ -65,19 +60,15 @@ class HMMOptimalPathLAI:
                     self.transitions[i].append(self.recomb_freq**2)
 
 
-    #function which creates an emissions matrix for a specific SNP (based on index in SNPseq) ###(move this down)
-    ### Need to adjust use of position depending on how we do our indexing (could be row # instead) ##########
-    def get_emissions_matrix(self,position):
+    def get_emissions_matrix(self,ind):
         """
-        This function takes a SNP position (on chr 21 ####), and uses the emission_df
-        to output a 4x16 emission probability matrix (rows = 4 ancestry pop combos,
-        cols = 16 genotype combos). The genotype combos are ordered lexicographically:
-        ["AA","AC","AG","AT","CA","CC","CG","CT","GA","GC","GG","GT","TA","TC","TG","TT"].
+        This function takes a SNP position index (works bc we have matched indexing for genotype
+        and emissions), and uses the emission_df to output a 4x16 emission probability matrix
+        (rows = 4 ancestry pop combos, cols = 16 genotype combos). The genotype combos are ordered
+        lexicographically: ["AA","AC","AG","AT","CA","CC","CG","CT","GA","GC","GG","GT","TA","TC",
+        "TG","TT"].
         """
-        #first grab the index of position in emission_df
-        ind = list(self.emissions_df.loc[:,"POS"]).index(position) ######## might change this
-
-        #now go through the columns with pop_nt freqs and make the emissions matrix
+        #go through the columns with pop_nt freqs and make the emissions matrix
         emissions_mat = np.zeros((4,16),dtype=float)
         for i in range(self.n_states):
             pop1 = self.pop_codes[self.states[i][0]]
@@ -92,36 +83,68 @@ class HMMOptimalPathLAI:
         return emissions_mat
 
 
-    ### TO DO: update this for new DSs
     def get_optimal_path(self):
-        for i, c in enumerate(self.text): ###
+        """
+        This function performs the Viterbi algorithm using the input sequence of SNPs.
+        For each SNP, a position (index) specific emissions matrix is calculated and then used
+        to calculate the highest possible probability of being in each state for each SNP.
+        A backtrack matrix is stored which will facilitate reconstruction of the most probable
+        path through states, and thus the haplotypes of a chromosome.
+        """
+        #go through every SNP in the input sequence (i=index, c=SNP string)
+        for i, c in enumerate(self.sequence):
+
+            #pull out the lexicographic indexing of the seen 2nt SNP (idx)
             idx = self.alphabet.index(c)
+            #create the emissions matrix for this SNP with the enumerated index
+            snp_emissions = self.get_emissions_matrix(i)
+
+            #go through all states and calculate max probability based on emission and transition probs
             for j in range(self.n_states):
-                emit_prob = self.emissions_dict[j][idx] ###
+
+                #get the probability of emitting c (SNP with index idx) when in state j
+                emit_prob = snp_emissions[j][idx]
+
+                #if this is the first SNP, then just set all positions in the matrix to the emissions prob
+                #(this assumes equal prob of start in any state, which is fine here)
                 if i == 0:
                     self.optimal_matrix[j, i] =  emit_prob
                     self.backtrack[j, i] = -1
+
                 else:
+                    #otherwise go through all possible previous states and keep track of transition
+                    #and emission probabilities from these states to the current one
                     probs = []
                     for previous in range(self.n_states):
-                        transition_prob = self.state_dict[previous][j]
+                        transition_prob = self.transitions[previous][j]
                         probs.append(emit_prob * transition_prob * self.optimal_matrix[previous, i-1])
+
+                    #select the max probability and record backtrack
                     max_prob = max(probs)
-                    back = probs.index(max_prob) ### think carefully about indexing here
+                    back = probs.index(max_prob)
                     self.optimal_matrix[j, i] = max_prob
                     self.backtrack[j, i] = back
 
-    ### TO DO: update this for new DSs
-    def reconstruct_path(self, state_names):
-        end_values = [self.optimal_matrix[v, len(self.text) - 1] for v in range(self.n_states)]
+                                        
+    def reconstruct_path(self):
+        """
+        This function uses the backtrack list stored from the Viterbi algorithm to reconstruct
+        the optimal path of states through the input sequence (SNPs).
+        """
+        #find the ending state (state with highest prob for final SNP) and add it to the path
+        end_values = [self.optimal_matrix[v, len(self.sequence) - 1] for v in range(self.n_states)]
         max_prob = max(end_values)
         end_state = end_values.index(max_prob)
         path = [end_state]
         previous_state = end_state
-        for i in range(len(self.text)-1, -1, -1): ###?
+
+        #now walk backwards through the backtrack list until reaching the start
+        for i in range(len(self.sequence)-1, -1, -1):
             #print(previous_state, i)
             path.append(int(self.backtrack[previous_state, i]))
             previous_state = int(self.backtrack[previous_state, i])
+
+        #reverse path and output with state names
         path.reverse()
-        output = [state_names[p] for p in path if p != -1]
+        output = [self.states[p] for p in path if p != -1]
         return output
