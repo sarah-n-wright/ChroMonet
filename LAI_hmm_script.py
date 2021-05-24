8c23701faaf4a99e4146a00b8cf5b2ce24e56f69
Hannah Mummey <hmmummey@gmail.com>
2021-05-24 16:00:52 -0700

    Added misc additional functions, changed path storage and output methods

--- LAI_hmm_script.py
+++ LAI_hmm_script.py
@@ -1,13 +1,45 @@
 import random as rn
 import numpy as np
+import pandas as pd
 
 
 def HammingDist(str1,str2):
+    """
+    This function computes the Hamming distance (edit distance) between 2 strings.
+    """
     dist = 0
     for i in range(len(str1)):
         if str1[i] != str2[i]:
             dist += 1
     return dist
+
+
+
+def makeSNPseq(df,col1,col2):
+    """
+    This function takes a dataframe containing a single individual's SNP genotypes
+    at various positions. It combines each row into one haplotype string (2nt) and
+    returns these as a list.
+    """
+    snps = []
+    for i in range(len(df)):
+        snps.append(df.iloc[i,col1]+df.iloc[i,col2])
+
+    return snps
+
+
+
+def standardizeIndices(df1,df2,col_name):
+    """
+    This function cuts down df1 to only contain values in the column, col_name that exist in
+    df2[,col_name]. It also checks that the two dataframes now have equal values in the two
+    columns. In practice, this is used to cut down an emission df to perfectly match an
+    individual's SNPs (in a genotype df) based on POS.
+    """
+    df2_pos = list(df2.loc[:,col_name])
+    fin_df1 = df1[df1[col_name].isin(df2_pos)]
+    print(df2_pos == list(fin_df1.loc[:,col_name]))
+    return fin_df1
 
 
 
@@ -38,6 +70,7 @@ class HMMOptimalPathLAI:
         self.current_state = rn.randint(0, self.n_states-1)
         self.optimal_matrix = np.zeros((self.n_states, len(SNPseq)))
         self.backtrack = np.zeros((self.n_states, len(SNPseq)))
+        self.final_path = []
 
 
     def get_transition_matrix(self):
@@ -93,7 +126,6 @@ class HMMOptimalPathLAI:
         """
         #go through every SNP in the input sequence (i=index, c=SNP string)
         for i, c in enumerate(self.sequence):
-
             #pull out the lexicographic indexing of the seen 2nt SNP (idx)
             idx = self.alphabet.index(c)
             #create the emissions matrix for this SNP with the enumerated index
@@ -101,7 +133,6 @@ class HMMOptimalPathLAI:
 
             #go through all states and calculate max probability based on emission and transition probs
             for j in range(self.n_states):
-
                 #get the probability of emitting c (SNP with index idx) when in state j
                 emit_prob = snp_emissions[j][idx]
 
@@ -111,21 +142,20 @@ class HMMOptimalPathLAI:
                     self.optimal_matrix[j, i] =  emit_prob
                     self.backtrack[j, i] = -1
 
+                #otherwise go through all possible previous states and keep track of transition
+                #and emission probabilities from these states to the current one
                 else:
-                    #otherwise go through all possible previous states and keep track of transition
-                    #and emission probabilities from these states to the current one
                     probs = []
                     for previous in range(self.n_states):
                         transition_prob = self.transitions[previous][j]
                         probs.append(emit_prob * transition_prob * self.optimal_matrix[previous, i-1])
-
                     #select the max probability and record backtrack
                     max_prob = max(probs)
                     back = probs.index(max_prob)
                     self.optimal_matrix[j, i] = max_prob
                     self.backtrack[j, i] = back
 
-                                        
+
     def reconstruct_path(self):
         """
         This function uses the backtrack list stored from the Viterbi algorithm to reconstruct
@@ -146,5 +176,27 @@ class HMMOptimalPathLAI:
 
         #reverse path and output with state names
         path.reverse()
-        output = [self.states[p] for p in path if p != -1]
-        return output
+        self.final_path = [self.states[p] for p in path if p != -1]
+
+
+    def output_path(self,chr_num):
+        """
+        This function translates the final path to a dataframe of the desired output
+        format: cols = CHR, POS, POP1, POP2. It then returns this dataframe so it can
+        be viewed and saved externally.
+        """
+        chrom = [chr_num] * len(self.final_path)
+        pos = list(self.emissions_df.loc[:,"POS"])
+
+        #reconstruct the two populations from the final path using the pop_codes dict
+        hap1 = []
+        hap2 = []
+        for gt in self.final_path:
+            pop1 = gt[0]
+            pop2 = gt[1]
+            hap1.append(self.pop_codes[pop1])
+            hap2.append(self.pop_codes[pop2])
+
+        df_dict = {'CHR':chrom, 'POS':pos, 'POP1':hap1, 'POP2':hap2}
+        results_df = pd.DataFrame(df_dict)
+        return(results_df)
